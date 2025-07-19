07ba62ca27610e70af7ac05152c929586aafc813
canoversaco <122825510+canoversaco@users.noreply.github.com>
2025-07-19 03:20:31 +0200

    Update images.js

--- src/components/images/images.js
+++ src/components/images/images.js
@@ -3,13 +3,22 @@ import Cali from "./cali.jpg";
 import Shem from "./shem.jpg";
 import Tilidin from "./tilidin.jpeg"
 import Schoko from "./schoko.jpg";
+import Gouda from "./gouda.jpg";
+import Mac from "./mac.jpg";
+import Kings from "./kings.jpg";
+import Runtz from "./runtz.jpg";
+import AK47 from "./ak47.jpg";
 const images = {
   defaultBild: DefaultBild,
   cali: Cali,
   schoko: Schoko,
   shem: Shem,
   tilidin: Tilidin,
-
+  gouda: Gouda,
+  mac: Mac,
+  kings: Kings,
+  runtz: Runtz,
+  ak47: AK47,
   // ...weitere Bilder...
 };
 
